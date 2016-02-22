21947b70a687432eb942702535a8dde78be4df70
Mahesh Bodapati <maheshbodapati90@gmail.com>
2016-02-22 09:06:32 +0000

    thread and pipe assembly

---
+++ src/thread/mips64/syscall_cp.s
@@ -0,0 +1,53 @@
+.set    noreorder
+
+.global __cp_begin
+.hidden __cp_begin
+.type   __cp_begin,@function
+.global __cp_end
+.hidden __cp_end
+.type   __cp_end,@function
+.global __cp_cancel
+.hidden __cp_cancel
+.type   __cp_cancel,@function
+.global __cp_cancel_data
+.hidden __cp_cancel_data
+.type   __cp_cancel_data,@function
+.hidden __cancel
+.global __syscall_cp_asm
+.hidden __syscall_cp_asm
+.type   __syscall_cp_asm,@function
+__syscall_cp_asm:
+__cp_begin:
+	lw	$4, 0($4)
+	bne	$4, $0, __cp_cancel    # __syscall_cp_asm has 8 arguments and $4 will contain the  
+        			       # cancellation flag and $5 is syscall number and passing remaining 
+        			       # 6 arguments to _cancel system call
+	move	$2, $5
+	move	$4, $6
+        move	$5, $7
+        move	$6, $8
+        move	$7, $9
+        move	$8, $10
+        move	$9, $11
+        ld	$10, 0($sp)
+	syscall
+__cp_end:
+	beq	$7, $0, 1f             # checking for summary overflow
+	nop
+	dsubu	$2, $0, $2
+1:	jr	$ra
+	nop
+__cp_cancel:				# if cancellation flag is 1 then call __cancel
+	move	$2, $ra
+.align 8
+	bal	1f
+	nop
+__cp_cancel_data:	
+        .gpdword __cp_cancel_data
+        .gpdword __cancel
+1:      ld	$3, ($ra)
+        dsubu	$3, $ra, $3
+        ld      $25, 8($ra)
+        daddu	$25, $25, $3
+	jr	$25
+	move	$ra, $2	
